4f547d989d5a966fb582aa4fc64a95e8323b04db
Kevin <lekev001@gmail.com>
2026-08-20 16:37:29 -0600

    [A] Custom Ability Tasks

---
+++ Source/Aura/Private/AbilityTask/TargetDataUnderMouseTask.cpp
@@ -0,0 +1,25 @@
+﻿// I made this :)
+
+
+#include "AbilityTask/TargetDataUnderMouseTask.h"
+
+
+UTargetDataUnderMouseTask* UTargetDataUnderMouseTask::CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility)
+{
+	UTargetDataUnderMouseTask* MyObj = NewAbilityTask<UTargetDataUnderMouseTask>(OwningAbility);
+	return MyObj;
+}
+
+void UTargetDataUnderMouseTask::Activate()
+{
+	FHitResult HitResult;
+	auto PlayerController = Ability->GetCurrentActorInfo()->PlayerController;
+	if (PlayerController.IsValid() == false)
+	{
+		UE_LOG(LogTemp, Error, TEXT("[%hs] PlayerController was invalid!"), __FUNCTION__);
+		return;
+	}
+	
+	PlayerController->GetHitResultUnderCursor(ECollisionChannel::ECC_Visibility, false, HitResult);
+	MouseLocationData.Broadcast(HitResult.Location);
+}
---
+++ Source/Aura/Public/AbilityTask/TargetDataUnderMouseTask.h
@@ -0,0 +1,29 @@
+﻿// I made this :)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Tasks/AbilityTask.h"
+#include "TargetDataUnderMouseTask.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FRetrievedDataSignature, const FVector&, Data);
+
+/**
+ * Retrieves some data under the mouse cursor at time of execution.
+ */
+UCLASS()
+class AURA_API UTargetDataUnderMouseTask : public UAbilityTask
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable, Category = "Ability|Tasks", meta=(HidePin = "OwningAbility", DefaultToSelf="OwningAbility", BlueprintInternalUseOnly, DisplayName = "TargetDataUnderMouse"))
+	static UTargetDataUnderMouseTask* CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility);
+	
+	UPROPERTY(BlueprintAssignable)
+	FRetrievedDataSignature MouseLocationData;
+	
+private:
+	virtual void Activate() override;
+	
+};
